a95181c8cb1bb0feb456419198faa0027ead65d5
Lorena <lorena.pantano@gmail.com>
2015-01-26 10:17:40 -0500

    add script to get profile of cluster

---
+++ pipeline/cluster_profile/get_profile.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+from collections import defaultdict
+import os, shutil
+from seqcluster import map_to_precursors
+
+
+def get_cluster(in_file, keep):
+    seqs = {}
+    loci = []
+    cluster_name = ""
+    with open(in_file) as in_handle:
+        for line in in_handle:
+            if line.startswith("C "):
+                if cluster_name == keep:
+                    return seqs, loci
+                seqs = {}
+                loci = []
+                cluster_name = line.strip().split(" ")[1]
+            if line.startswith("L "):
+                loci = line.strip().split(" ")[2:]
+            if line.startswith("S "):
+                s, f = line.strip().split(" ")[1:2]
+                seqs[s] = f
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Check installation")
+    parser.add_argument("--json", required=True, help="json file.")
+    parser.add_argument("--keep", required=True, help="cluster id.")
+    parser.add_argument("--ref", help="Path to aligner index.")
+    parser.add_argument("--out", help="output file.")
+
+    args = parser.parse_args()
+
+    seqs, loci = get_cluster(args.json, args.keep)
+    map_to_precursors(seqs.keys(), seqs.values(), {args.keep: loci}, args)
